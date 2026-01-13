c6d32de1fb18990374e778dafd2c04055ae1c6e0
Ishaan2006-ishu <vars.ishaan2006@gmail.com>
2026-01-13 06:37:43 +0530

    My First Server

---
+++ SERVER/index.js
@@ -0,0 +1,16 @@
+const http=require("http");
+const fs=require("fs");
+
+
+const myServer=http.createServer((req,res)=>{
+    const log=`${Date.now()}:New Req Received\n `
+    fs.appendFile("log.txt",log,(err,data)=>{
+    res.end("hello from server")
+
+    }
+        
+    );
+    
+});
+
+myServer.listen(8000,()=>console.log("server started"))
