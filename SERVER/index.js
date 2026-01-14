bb248d3a8cc3b435931958cc6b6188950bb33a3d
Ishaan2006-ishu <vars.ishaan2006@gmail.com>
2026-01-14 07:13:53 +0530

    http method

--- SERVER/index.js
+++ SERVER/index.js
@@ -5,17 +5,29 @@ const url=require("url");
 
 const myServer=http.createServer((req,res)=>{
     if(req.url==="/favicon.ico") return res.end();
-    const log=`${Date.now()}: ${req.url} New Req Received\n `
+    const log=`${Date.now()}: ${req.method} ${req.url} New Req Received\n `
     const myUrl=url.parse(req.url,true);
     console.log(myUrl);
     fs.appendFile("log.txt",log,(err,data)=>{
         switch(myUrl.pathname){
             case "/":
-                res.end("homPage");
+                if(req.method==='GET'){
+                    res.end("homPage");
+
+                }
+                
                 break;
             case "/about":
                 const username=myUrl.query.myName;
                 res.end(`hi, ${username}`);
+                break;
+            case "/signup":
+                if (req.method==="GET"){
+                    res.end("this is a signup form");
+                }
+                else if (req.method==="POST"){
+                    res.end("FORM IS FILLED SUCCESSFULLY");
+                }
                 break;
             default:
                 res.end("404 not found");
