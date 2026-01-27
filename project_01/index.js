e66ce8634eb23c8564a27a3d2b503bf534b17007
Ishaan2006-ishu <vars.ishaan2006@gmail.com>
2026-01-27 07:27:24 +0530

    start postmam

--- project_01/index.js
+++ project_01/index.js
@@ -1,72 +1,81 @@
-// const express=require("express");
-// const users=require("./MOCK_DATA.json");
+const express=require("express");
+const users=require("./MOCK_DATA.json");
+const fs=require("fs");
+//console.log(users);
 
+const app=express();
+app.use(express.urlencoded({extended: false}));
+const PORT=8000;
+
+
+//ROUTES
+app.get('/users', (req,res)=>{
+    const html =`
+    <ul>
+    ${users.map(user=>`<li>${user.first_name}</li>`).join("")}
+    </ul>`;
+    return res.send(html);
+
+})
+app.get('/api/users', (req,res)=>{
+    return res.send(users);
+
+
+});
+app.get("/api/users/:id",(req,res)=>{
+    
+    const id=Number(req.params.id);
+    const user = users.find((user)=>user.id === id);
+    res.json(user);
+
+})
+
+app.post("/api/users", (req, res)=>{
+    //create todo: create new user
+
+    const body=req.body;
+    users.push({...body,id:users.length+1});
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
+        return res.json({status:"success", id:users.length});
+    })
+    
+})
+app.patch("/api/users:id", (req, res)=>{
+    //create tood: cedit user id
+    return res.json("status:pending");
+})
+app.delete("/api/users", (req, res)=>{
+    //create tood:delete user with thaat id
+    return res.json("status:pending");
+})
+
+
+
+app.listen(PORT, ()=>console.log(`Server startd at port ${8000}`));
+
+// const express = require("express");
 // const app=express();
-// const PORT=8000;
 
-
-// //ROUTES
-// app.get('/users', (req,res)=>{
-//     const html =`
+// const data=require("./Books.json");
+// app.get("/books",(req,res)=>{
+//     const html=`
 //     <ul>
-//     ${users.map(user=>`<li>${user.first_name}</li>`).join("")}
-//     </ul>`;
-//     return res.send(html);
-
-// })
-// app.get('/api/users', (req,res)=>{
-//     return res.send(users);
-
+//     ${data.map(book=>`<li>${book.name}.</li>`).join("")}
+//     </ul>
+//     `
+//     res.send(html);
 
 // });
-// app.get("/api/users/:id",(req,res)=>{
-    
+
+// app.get("/books/:id",(req,res)=>{
 //     const id=Number(req.params.id);
-//     const user = users.find((user)=>user.id === id);
-//     res.json(user);
+//     const book=data.find(book=>(book.id===id));
+//     //res.json(book);
+//     const html=`Available -> ${book.name}`;
+//     res.send(html);
 
-// })
-
-// app.post("/api/users", (req, res)=>{
-//     //create todo: create new user
-//     return res.json("status:pending");
-// })
-// app.patch("/api/users:id", (req, res)=>{
-//     //create tood: cedit user id
-//     return res.json("status:pending");
-// })
-// app.delete("/api/users", (req, res)=>{
-//     //create tood:delete user with thaat id
-//     return res.json("status:pending");
-// })
+// });
 
 
 
-// app.listen(PORT, ()=>console.log(`Server startd at port ${8000}`));
-
-const express = require("express");
-const app=express();
-
-const data=require("./Books.json");
-app.get("/books",(req,res)=>{
-    const html=`
-    <ul>
-    ${data.map(book=>`<li>${book.name}.</li>`).join("")}
-    </ul>
-    `
-    res.send(html);
-
-});
-
-app.get("/books/:id",(req,res)=>{
-    const id=Number(req.params.id);
-    const book=data.find(book=>(book.id===id));
-    //res.json(book);
-    const html=`Available -> ${book.name}`;
-    res.send(html);
-
-});
-
-
-
-app.listen(8000)
+// app.listen(8000)
